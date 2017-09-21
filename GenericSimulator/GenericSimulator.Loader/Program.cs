c82fc015a746caf6043ac32222501a9b68aa7415
Justin Wendlandt <jwendl@hotmail.com>
2017-09-21 00:27:28 -0700

    Added files.

---
+++ GenericSimulator/GenericSimulator.Loader/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSimulator.Loader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "Endpoint=sb://jwdatasimulator.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ABsRy3NJ7vY3088/qExVpTkrRub18sAU/pa5NzpNkIE=";
+            var queueName = "eventdataqueue";
+
+            var client = new QueueClient(connectionString, queueName);
+            var message = new Message(Encoding.UTF8.GetBytes("test"));
+
+            await client.SendAsync(message);
+
+            Console.WriteLine("Message successfully sent! Press ENTER to exit program");
+            Console.ReadLine();
+        }
+    }
+}
